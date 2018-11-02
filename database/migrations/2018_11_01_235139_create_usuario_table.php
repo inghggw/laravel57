5fda0accfc7147446d9350ade599e6f00ed5260d
inghggw <inghggw@10.9.22.42>
2018-11-01 21:33:28 -0500

    Migraciones usuario y estado

---
+++ database/migrations/2018_11_01_235139_create_usuario_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('primer_nombre',100);
+            $table->string('segundo_nombre',100)->nullable();
+            $table->string('primer_apellido',100);
+            $table->string('segundo_apellido',100)->nullable();
+            $table->string('correo',150)->unique();
+            $table->text('password');
+            $table->date('fecha_nacimiento')->nullable();
+            $table->unsignedTinyInteger('estado_id');
+            $table->timestamps();
+
+            $table->foreign('estado_id')->references('id')->on('estado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
